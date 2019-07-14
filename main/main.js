bd43eef5c306b6c54ed0fd437ef7cf148fd11fbf
Dallin0930 <2869865192@qq.com>
2019-07-14 23:15:27 +0800

    POs_1.0_implement_but_doesnot_test_because_of_the_environment

--- main/main.js
+++ main/main.js
@@ -1,3 +1,182 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+//生成Tags对象数组
+var decodeBarcodes = (tags) =>{
+  tags.reduce((result,element) =>{
+    let ch = element.includes('-') ? element.split('-')[0] : element;
+    let num = element.includes('-') ? parseFloat(element.split('-')[1])  : 1;
+    result.find(x => x.key == ch)?
+    result.find(x => x.key === ch).count+=num 
+    :result.push({key:ch,count:num}); 
+    return result;     
+  },[])    
+}
+
+
+//加载原始数据
+function loadAllItems() {
+  return [
+    {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000001',
+      name: '雪碧',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000002',
+      name: '苹果',
+      unit: '斤',
+      price: 5.50
+    },
+    {
+      barcode: 'ITEM000003',
+      name: '荔枝',
+      unit: '斤',
+      price: 15.00
+    },
+    {
+      barcode: 'ITEM000004',
+      name: '电池',
+      unit: '个',
+      price: 2.00
+    },
+    {
+      barcode: 'ITEM000005',
+      name: '方便面',
+      unit: '袋',
+      price: 4.50
+    }
+  ];
+}
+
+
+
+//由字典匹配原始数据，生成含count的对象数组  
+var combineItems= (tags) =>{
+  let  decodeBarcodes= decodeBarcodes(tags);
+  const allItems=loadAllItems();
+  
+  var items={};   
+
+  allItems.map((x,decodeBarcodes) =>{
+      decodeBarcodes.key == x.barcode;
+      let item_form={
+        barcode:x.barcode,
+        name:x.name,
+        unit:x.unit,
+        price:x.price,
+        count:decodeBarcodes[key][value]
+      }
+      items.push(item_form);
+  }
+  ) 
+      return items;  
+}
+
+
+//由原始输入Tags，得到Items对象数组输出``````````````````````
+var decodeTags = tags =>{
+     return  combineItems(tags);
+}
+
+//加载促销信息
+function loadPromotions() {
+  return [
+    {
+      type: 'BUY_TWO_GET_ONE_FREE',
+      barcodes: [
+        'ITEM000000',
+        'ITEM000001',
+        'ITEM000005'
+      ] 
+    } 
+  ]; 
+}
+
+//由Items和促销方式计算单个商品小计
+var promotionReceiptItems=(tags) =>{
+  let items = decodeTags(tags);
+  let promotions = loadPromotions();
+  
+  var receiptitems={};
+  items.fiter(
+     (itemlist,promotions) => {
+         if(c.barcode in promotions[0].barcodes)
+         {
+            let item_form={
+              barcode:itemlist.barcode,
+              name:itemlist.name,
+              unit:itemlist.unit,
+              price:itemlist.price,
+              count:decodeBarcodes[key][value],
+              subtotal:itemlist.count/3*itemlist.price*2+itemlist%3*itemlist.price
+            }
+         }
+         receiptitems.push(item_form);
+     }
+  )
+    return receiptitems;
+}
+
+//由输入Items，输出receiptitems``````````````````````````
+var calculateReceiptItems= (tags) =>{
+     return promotionReceiptItems(tags);
+}  
+
+//由输入receiptitems，得到总计totalnumber
+var calculateReceiptTotal=(tags) =>{
+    let receiptitems=promotionReceiptItems(tags);
+    
+    let total=receiptitems.map(receiptitems.subtotal).reduce();
+    
+    return total;
+}
+
+
+//由输入receiptitems，得到节省的钱
+var calculatereceiptSaving=(tags) =>{
+  let receiptitems=promotionReceiptItems(tags);
+  return receiptitems.price * receiptitems.count - calculateReceiptTotal(tags);
+ 
+}
+
+//得到receipt ``````````````````````````````
+var calculateReceipt=(tags) =>{
+   let receiptitems=calculateReceiptItems(tags);
+   let total=calculateReceiptTotal(tags);
+   let saving=calculatereceiptSaving(tags);
+
+   let receipt=" ";
+   receipt += 
+    `***<没钱赚商店>收据***
+`;
+    for (let i = 0; i < receiptitems.length; i++) {
+        receipt += `名称：${receiptitems[i].name}，数量：${receiptitems[i].count}${receiptitems[i].unit}，单价：${receiptitems[i].price}(元)，小计：${receiptitems[i].subtotal}(元)
+`;
+    }
+    receipt += `----------------------
+`;
+    receipt += "总计："+ total +"(元)"+"\n"+"节省："+"saving"+"(元)"+"\n";
+    receipt += `**********************`;
+    return receipt ;
+}
+
+
+//printReceipt打印
+var printReceipt= (tags) =>{
+
+  return calculateReceipt(tags) ;
+}
+
+
+
+exports=[loadAllItems,decodeBarcodes,combineItems,decodeTags,
+         loadPromotions,promotionReceiptItems,calculateReceiptItems,
+         calculateReceiptTotal,calculatereceiptSaving,calculateReceipt,
+         printReceipt];
--- test/main-test.js
+++ test/main-test.js
@@ -1,25 +1,29 @@
 'use strict';
 
-describe('pos', () => {
+const loadAllItems = require('../main')[0];
+const decodeBarcodes = require('../main')[1];
+const combineItems = require('../main')[2];
+const decodeTags = require('../main')[3];
+const loadPromotions = require('../main')[4];
+const promotionReceiptItems = require('../main')[5];
+const  calculateReceiptItems= require('../main')[6];  
+const calculateReceiptTotal = require('../main')[7];
+const calculatereceiptSaving = require('../main')[8];         
+const calculateReceipt = require('../main')[9];     
+const printReceipt = require('../main')[10];   
 
-  it('should print text', () => {
+const tags = [
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000003-2.5',  
+  'ITEM000005',  
+  'ITEM000005-2',
+];
 
-    const tags = [
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000003-2.5',
-      'ITEM000005',
-      'ITEM000005-2',
-    ];
-
-    spyOn(console, 'log');
-
-    printReceipt(tags);
-
-    const expectText = `***<没钱赚商店>收据***
+const expectText = `***<没钱赚商店>收据***
 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
 名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
 名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
@@ -28,6 +32,45 @@ describe('pos', () => {
 节省：7.50(元)
 **********************`;
 
-    expect(console.log).toHaveBeenCalledWith(expectText);
+
+/*describe('pos', () => {
+
+  it('should print text', () => {
+
+    /* const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',  
+      'ITEM000001',
+      'ITEM000003-2.5',  
+      'ITEM000005',
+      'ITEM000005-2',
+    ]; */  
+
+   // spyOn(console, 'log'); 
+
+    /*printReceipt(tags);    
+
+    const expectText = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：58.50(元)
+节省：7.50(元)
+**********************`;    
+
+    expect(console.log).toHaveBeenCalledWith(expectText);  
   });
-});
+
+
+});*/
+
+
+//测试1：统计ID count 
+const count={"ITEM000001":5,"ITEM000003":2.5,"ITEM000005":3};     
+it('should return true when decodeBarcodes equal exceptedresult', () => {  
+    expect(decodeBarcodes(tags)).toStrictEqual(count);        
+}); 
+
